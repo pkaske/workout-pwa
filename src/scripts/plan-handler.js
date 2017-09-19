8306f89c40c17afca628bb0582fb3ab9528e7766
Peter Kaske <p.kaske@gmail.com>
2017-09-20 00:58:06 +0200

    WIP: Manage workout plans
    Also: Layout and UX improvements.

---
+++ src/scripts/plan-handler.js
@@ -0,0 +1,58 @@
+class PlanHandler {
+  constructor() {
+    this._planDb = new PouchDB('plans');
+
+    this._planDb.createIndex({
+      index: { fields: ['name'] }
+    }).then(() => {
+      this.all().then(docs => {
+        this._fire('plans-changed', docs);
+      })
+    });
+
+    this._changeFeed();
+  }
+
+  create(doc) {
+    const exercise = doc;
+    return this._planDb.post(exercise);
+  }
+
+  edit(doc) {
+    return this._planDb.put(doc);
+  }
+
+  all() {
+    return this._planDb.find({
+      selector: {
+        name: { $gte: null }
+      },
+      sort: [ 'name' ]
+    });
+  }
+
+  deleteById(id) {
+    return this._planDb.get(id)
+      .then(doc => this._planDb.remove(doc))
+      .then(() => this.all()).then(docs => {
+        this._fire('plans-changed', docs);
+      });
+  }
+
+  _changeFeed() {
+    this._planDb.changes({
+      since: 'now',
+      live: true
+    }).on('change', () => {
+      this.all().then(docs => {
+        this._fire('plans-changed', docs);
+      });
+    });
+  }
+
+  _fire(name, detail) {
+    document.dispatchEvent(
+      new CustomEvent(name, { detail: detail, bubbles: false, composed: false })
+    );
+  }
+}
