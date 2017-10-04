a3cb66f97cd6ef73aaf92efd87256064f3ada791
Peter Kaske <p.kaske@gmail.com>
2017-10-04 09:17:02 +0200

    Rewrite handlers to es5 because uglify of polymer-build doesn't support es6.

--- src/scripts/exercise-handler.js
+++ src/scripts/exercise-handler.js
@@ -1,94 +1,93 @@
 /* eslint no-console: off */
 /* global PouchDB */
+/* global ExerciseHandler */
 
-class ExerciseHandler {
-  constructor() {
-    this._exercisedDb = new PouchDB('exercises');
+window.ExerciseHandler = function() {
+  this._exercisedDb = new PouchDB('exercises');
 
-    this._exercisedDb.createIndex({
-      index: { fields: [ 'name' ] }
-    })
-      .then(() => this.all())
-      .then(result => {
-        if (result.docs.length === 0) {
-          this._setupDefaultExercises();
-        } else {
-          this._fire('exercises-changed', result);
-        }
-      });
-
-    this._changeFeed();
-  }
-
-  _setupDefaultExercises() {
-    fetch('src/default-exercises.json')
-      .then(response => {
-        const contentType = response.headers.get('content-type');
-        if (contentType && contentType.includes('application/json')) {
-          return response.json();
-        }
-        throw Error('Exercises file invalid');
-      })
-      .then(list => {
-        return Promise.all(list.map(ex => this.create(ex)));
-      })
-      .then(() => {
-        console.info('Default exercises created');
-      })
-      .catch(err => {
-        console.warn(err.message);
-      });
-  }
-
-  create(doc) {
-    const exercise = doc;
-    return this._exercisedDb.post(exercise);
-  }
-
-  edit(doc) {
-    return this._exercisedDb.put(doc);
-  }
-
-  all() {
-    return this._exercisedDb.find({
-      selector: {
-        name: { $gte: null }
-      },
-      fields: [ '_id', 'name' ],
-      sort: [ 'name' ]
-    });
-  }
-
-  findWithRecords(ids) {
-    return this._exercisedDb.find({
-      selector: {
-        _id: { $in: ids }
+  this._exercisedDb.createIndex({
+    index: { fields: [ 'name' ] }
+  })
+    .then(() => this.all())
+    .then(result => {
+      if (result.docs.length === 0) {
+        this._setupDefaultExercises();
+      } else {
+        this._fire('exercises-changed', result);
       }
     });
-  }
 
-  deleteById(id) {
-    return this._exercisedDb.get(id)
-      .then(doc => this._exercisedDb.remove(doc))
-      .then(() => this.all()).then(result => {
-        this._fire('exercises-changed', result);
-      });
-  }
+  this._changeFeed();
+};
 
-  _changeFeed() {
-    this._exercisedDb.changes({
-      since: 'now',
-      live: true
-    }).on('change', () => {
-      this.all().then(result => {
-        this._fire('exercises-changed', result);
-      });
+ExerciseHandler.prototype._setupDefaultExercises = function() {
+  fetch('src/default-exercises.json')
+    .then(response => {
+      const contentType = response.headers.get('content-type');
+      if (contentType && contentType.includes('application/json')) {
+        return response.json();
+      }
+      throw Error('Exercises file invalid');
+    })
+    .then(list => {
+      return Promise.all(list.map(ex => this.create(ex)));
+    })
+    .then(() => {
+      console.info('Default exercises created');
+    })
+    .catch(err => {
+      console.warn(err.message);
     });
-  }
+};
 
-  _fire(name, detail) {
-    document.dispatchEvent(
-      new CustomEvent(name, { detail: detail, bubbles: false, composed: false })
-    );
-  }
-}
+ExerciseHandler.prototype.create = function(doc) {
+  const exercise = doc;
+  return this._exercisedDb.post(exercise);
+};
+
+ExerciseHandler.prototype.edit = function(doc) {
+  return this._exercisedDb.put(doc);
+};
+
+ExerciseHandler.prototype.all = function() {
+  return this._exercisedDb.find({
+    selector: {
+      name: { $gte: null }
+    },
+    fields: [ '_id', 'name', '_rev' ],
+    sort: [ 'name' ]
+  });
+};
+
+ExerciseHandler.prototype.findWithRecords = function(ids) {
+  return this._exercisedDb.find({
+    selector: {
+      _id: { $in: ids }
+    }
+  });
+};
+
+ExerciseHandler.prototype.deleteById = function(id) {
+  return this._exercisedDb.get(id)
+    .then(doc => this._exercisedDb.remove(doc))
+    .then(() => this.all()).then(result => {
+      this._fire('exercises-changed', result);
+    });
+};
+
+ExerciseHandler.prototype._changeFeed = function() {
+  this._exercisedDb.changes({
+    since: 'now',
+    live: true
+  }).on('change', () => {
+    this.all().then(result => {
+      this._fire('exercises-changed', result);
+    });
+  });
+};
+
+ExerciseHandler.prototype._fire = function(name, detail) {
+  document.dispatchEvent(
+    new CustomEvent(name, { detail: detail, bubbles: false, composed: false })
+  );
+};
--- src/scripts/plan-handler.js
+++ src/scripts/plan-handler.js
@@ -1,60 +1,59 @@
 /* eslint no-console: off */
 /* global PouchDB */
+/* global PlanHandler */
 
-class PlanHandler {
-  constructor() {
-    this._planDb = new PouchDB('plans');
+window.PlanHandler = function() {
+  this._planDb = new PouchDB('plans');
 
-    this._planDb.createIndex({
-      index: { fields: ['name'] }
-    })
-      .then(() => this.all())
-      .then(result => {
-        this._fire('plans-changed', result);
-      });
-
-    this._changeFeed();
-  }
-
-  create(doc) {
-    return this._planDb.post(doc);
-  }
-
-  edit(doc) {
-    return this._planDb.put(doc);
-  }
-
-  all() {
-    return this._planDb.find({
-      selector: {
-        name: { $gte: null }
-      },
-      sort: [ 'name' ]
+  this._planDb.createIndex({
+    index: { fields: ['name'] }
+  })
+    .then(() => this.all())
+    .then(result => {
+      this._fire('plans-changed', result);
     });
-  }
 
-  deleteById(id) {
-    return this._planDb.get(id)
-      .then(doc => this._planDb.remove(doc))
-      .then(() => this.all()).then(result => {
-        this._fire('plans-changed', result);
-      });
-  }
+  this._changeFeed();
+};
 
-  _changeFeed() {
-    this._planDb.changes({
-      since: 'now',
-      live: true
-    }).on('change', () => {
-      this.all().then(result => {
-        this._fire('plans-changed', result);
-      });
+PlanHandler.prototype.create = function(doc) {
+  return this._planDb.post(doc);
+};
+
+PlanHandler.prototype.edit = function(doc) {
+  return this._planDb.put(doc);
+};
+
+PlanHandler.prototype.all = function() {
+  return this._planDb.find({
+    selector: {
+      name: { $gte: null }
+    },
+    sort: [ 'name' ]
+  });
+};
+
+PlanHandler.prototype.deleteById = function(id) {
+  return this._planDb.get(id)
+    .then(doc => this._planDb.remove(doc))
+    .then(() => this.all()).then(result => {
+      this._fire('plans-changed', result);
     });
-  }
+};
 
-  _fire(name, detail) {
-    document.dispatchEvent(
-      new CustomEvent(name, { detail: detail, bubbles: false, composed: false })
-    );
-  }
-}
+PlanHandler.prototype._changeFeed = function() {
+  this._planDb.changes({
+    since: 'now',
+    live: true
+  }).on('change', () => {
+    this.all().then(result => {
+      this._fire('plans-changed', result);
+    });
+  });
+};
+
+PlanHandler.prototype._fire = function(name, detail) {
+  document.dispatchEvent(
+    new CustomEvent(name, { detail: detail, bubbles: false, composed: false })
+  );
+};
